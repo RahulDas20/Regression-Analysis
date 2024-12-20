2379e655f7e54ec2414ae01a811c97bb4ac6eb9a
RahulDas20 <rahulbd6546@gmail.com>
2024-12-20 20:51:49 +0600

    Initial Commit to local Branch

--- Bases_or_stolen_bases.R
+++ Bases_or_stolen_bases.R
@@ -5,7 +5,7 @@ ds_theme_set()
 
 
 
-#Scatter plot between Homerun and Run
+#Scatter plot between Home run and Run
 Teams %>% filter(yearID %in% 1961:2001) %>%
   mutate(HR_per_game = HR/G, R_per_game = R/G) %>%
   ggplot(aes(HR_per_game, R_per_game)) +
@@ -28,4 +28,14 @@ Teams %>% filter(yearID %in% 1961 : 2001) %>%
 Teams %>% filter( yearID %in% 1961:2001) %>%
   mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
   ggplot(aes(AB_per_game, R_per_game)) +
+  geom_point(alpha = 0.5)
+
+## This is my Branch!!
+## Nobody will enter here!!
+
+
+#Triples per game vs Doubles per game
+Teams %>% filter( yearID %in% 1961:2001) %>%
+  mutate(TR_per_game = X3B/G, DR_per_game = X2B/G) %>%
+  ggplot(aes(TR_per_game, DR_per_game)) +
   geom_point(alpha = 0.5)
