a9f9e6098d385498c737e51638d82855ed6ef002
RahulDas20 <rahulbd6546@gmail.com>
2024-12-24 11:11:15 +0600

    question commit

--- Galtonheights.R
+++ Galtonheights.R
@@ -34,18 +34,16 @@ galton_heights %>% summarize(cor(father,son))
 
 
 #compute sample correlation
-my_sample <- slice_sample(galton_heights, n = 25)
-
-
-R <- my_sample %>% summarize(cor(father, son))
-
+R <- sample_n(galton_heights, 25, replace = TRUE) %>%
+  summarize(r = cor(father, son)) %>% pull(r)
+R
 
 #Monte carlo simulation to show the distribution of the corellation
 B <- 10000
 N <- 25
 R <- replicate(B, {
   slice_sample(galton_heights, n = N, replace = TRUE) %>%
-    summarize( r = cor(father, son)) %>% .$r
+    summarize( r = cor(father, son)) %>%  pull(r)
 })
 
 data_frame(R) %>% ggplot(aes(R))  + geom_histogram(binwidth = 0.05, color = "black")
@@ -57,9 +55,27 @@ mean(R)
 #standard error is high relative to its size
 sd(R)
 
-#qq plot to evaluate wheather N is large enough
+#qq plot to evaluate whether N is large enough
 data_frame(R) %>% ggplot(aes(sample = R)) +
   stat_qq() +
   geom_abline(intercept = mean(R), slope = sqrt((1-mean(R)^2)/(N-2)))
-               
-              
+
+
+library(Lahman)
+
+my_team <- Teams %>% filter(yearID %in% 1961:2001)
+
+#calculate the wins per game
+win_rate <- my_team$W/my_team$G
+
+#calculate the error per game
+error_per_game <- my_team$E / my_team$G
+
+#calculate the doubles per game
+double_per_game <- my_team$X2B/ my_team$G
+
+#calculate the tripples per game
+tripples_per_game <- my_team$X3B / my_team$G
+
+#calculate the correlation
+cor(double_per_game, tripples_per_game)
